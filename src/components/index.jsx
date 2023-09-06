d310ea4954273c70d32661cc9c86f9954446af6d
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-09-07 01:04:44 +0530

    add: component structure

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Alert from "./Alert";
+import Avatar from "./Avatar";
+import Badge from "./Badge";
+import Button from "./Button";
+import Card from "./Card";
+
+export { Alert, Avatar, Badge, Button, Card };
